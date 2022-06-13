f97e476506e719125c98149cb356a97d96b6960c
Amit Naudiyal <amit.naudiyal@yahoo.com>
2022-06-13 23:35:12 +1000

    changes in ecr

--- base/ecr.tf
+++ base/ecr.tf
@@ -12,17 +12,6 @@ locals {
   ecr_url = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.prefix}-"
 }
 
-resource "aws_ssm_parameter" "ecr" {
-  name = "/${var.prefix}/base/ecr"
-  value = local.ecr_url
-  type  = "String"
-}
-
-resource "local_file" "ecr" {
-  filename = "${path.module}/../ecr-url.txt"
-  content = local.ecr_url
-}
-
 output "repository_base_url" {
   value = local.ecr_url
 }
